96ee28efc7f00d6727b9dfb9a897b5d20caf93c3
kodowicz <annakodowicz@gmail.com>
2020-05-04 22:42:25 +0200

    styled background with confetti

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import confetti from '../assets/images/confetti.svg';
 
 const BGWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  height: 100vh;
+  min-height: 100vh;
+  height: auto;
   background: linear-gradient(180deg, #6837FA 0%, #663CE2 100%);
   width: 100%;
-  z-index: -1;
+  ${'' /* z-index: -2; */}
 `;
 
-const Background = () => <BGWrapper />;
+const Confetti = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-image: ${props => `url(${props.svg})`};
+  ${'' /* z-index: -1 */}
+`
+
+
+const Background = (props) => (
+  <BGWrapper>
+    <Confetti svg={confetti}>
+      { props.children }
+    </Confetti>
+  </BGWrapper>
+);
 
 export default Background
