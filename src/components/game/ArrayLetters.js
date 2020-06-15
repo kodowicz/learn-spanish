558878bf1678cea4b855ad6aca3bf3ca1627a4ac
kodowicz <annakodowicz@gmail.com>
2020-06-15 22:50:12 +0200

    styled components and improved user interactions

--- src/components/game/ArrayLetters.js
+++ src/components/game/ArrayLetters.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+
 
 class ArrayLetters extends Component {
   state = {
-    isChosen: false,
+    definition: "",
+    correctAnswer: "",
     counter: 0,
-    answer: [],
     index: 0,
-    letters: [],
+    lastUserLetter: null,
+    isFinished: false,
+    answer: [],
+    letters: []
   }
 
   componentDidMount() {
@@ -17,28 +22,49 @@ class ArrayLetters extends Component {
     this.setState((prevState, props) => {
       const { item } = this.props;
       const { index } = this.state;
+      const definition = item.definition;
       const correctAnswer = item.term;
+      const regex = /(\(|\)|\s|\n)/;
+
+      let answer = correctAnswer
+        .split("")
+        .map(letter => regex.test(letter) ? letter : null);
+
       let letters = this.randomLetters(item.term, index);
       letters = this.shuffleOptions(letters);
 
+      const lastUserLetter = answer.lastIndexOf(null);
+
       return {
+        definition,
         letters,
-        correctAnswer
+        answer,
+        correctAnswer,
+        lastUserLetter
       }
     })
   }
 
   randomLetters = (word, index) => {
-    const letter = word[index];
+    const correctLetter = {
+      letter: word[index],
+      isWrong: false,
+      isScaled: true
+    };
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'z'];
-    let letters = [letter];
+    let letters = [correctLetter];
 
     while (letters.length < 5) {
       const index = Math.floor(Math.random() * alphabet.length);
-      const isTaken = letters.some(letter => letter === alphabet[index]);
+      const isTaken = letters.some(el => el.letter === alphabet[index]);
 
       if (!isTaken) {
-        letters.push(alphabet[index])
+        const wrongLetter = {
+          letter: alphabet[index],
+          isWrong: false,
+          isScaled: true
+        }
+        letters.push(wrongLetter)
       }
     }
 
@@ -60,101 +86,333 @@ class ArrayLetters extends Component {
     return newOrder;
   }
 
+  getExcludedIndexes = (word) => {
+    const regex = /[$&+,:;=?@#|'<>.^*()%!-\s]/g
+    const array = [];
+    let match;
+
+    while ((match = regex.exec(word)) != null) {
+      array.push(match.index)
+    }
+
+    return array
+  }
+
   handlePicking = (event) => {
-    const choice = event.target.id;
+    event.persist();
 
     this.setState((prevState, props) => {
-      const correctLetter = prevState.correctAnswer[prevState.index];
+      const correctAnswer = prevState.correctAnswer;
+      const correctLetter = correctAnswer[prevState.index];
+      const choice = event.target.id;
       let nextIndex = prevState.index + 1;
-      let answer = prevState.answer;
+      let answer = [...prevState.answer];
+      let trimNumber = 1;
+      let insertedAnswer = [choice];
 
+      // last answer letter; don't pick a new array of random letters
+      if (prevState.lastUserLetter === prevState.index) {
+        const emptyIndex = answer.findIndex(letter => !letter);
+        answer.splice(emptyIndex, trimNumber, ...insertedAnswer);
 
-      if (choice === correctLetter) {
-        const nextLetter = prevState.correctAnswer[nextIndex];
-
-        // we don't want to choose space
-        if (nextLetter === " ") {
-          nextIndex = prevState.index + 2;
-          answer.push(choice, nextLetter)
-
-        } else if (nextLetter) {
-          answer.push(choice)
-
-        // finished game
-        } else {
-          return ({
-            isChosen: true
-          })
+        return {
+          answer,
+          index: nextIndex
         }
 
+      } else if (choice === correctLetter) {
+        const excludedIndexes = this.getExcludedIndexes(correctAnswer);
+        const nextLetter = correctAnswer[nextIndex];
+
+        if (excludedIndexes.indexOf(nextIndex) !== -1) {
+
+          while (excludedIndexes.indexOf(nextIndex) !== -1) {
+            insertedAnswer.push(prevState.correctAnswer[nextIndex]);
+            trimNumber++;
+            nextIndex++;
+          }
+
+          const emptyIndex = answer.findIndex(letter => !letter);
+          answer.splice(emptyIndex, trimNumber, ...insertedAnswer)
+
+        } else if (nextLetter) {
+          const emptyIndex = answer.findIndex(letter => !letter);
+          answer.splice(emptyIndex, trimNumber, ...insertedAnswer);
+        }
+
+        // create a new array of random letters
         let letters = this.randomLetters(prevState.correctAnswer, nextIndex);
         letters = this.shuffleOptions(letters);
 
-        return({
-          index: nextIndex,
+        return {
           letters,
-          answer
-        })
+          answer,
+          index: nextIndex,
+          counter: 0
+        }
 
+      // wrong letter
       } else {
-        alert('wrong');
         let counter = prevState.counter + 1;
+        let letters = prevState.letters;
+        const wrongIndex = letters.findIndex(element => element.letter === choice);
+        let wrongLetter = letters[wrongIndex];
 
-        // game over
-        if (counter >= 3) {
-          props.chooseOption(props.item, false)
+        // shake letter
+        wrongLetter.isWrong = true;
+        letters.splice(wrongIndex, 1, wrongLetter);
 
-          return ({
-            isChosen: true,
-            counter
-          })
-
-        } else {
-          return({
-            counter
-          })
+        return {
+          counter,
+          letters
         }
       }
 
     }, () => {
-      const { counter } = this.state;
-      const { item, chooseOption} = this.props;
+      const { item, counter } = this.state;
+      const { chooseOption } = this.props;
 
       if (counter === 3) {
         chooseOption(item, false)
+      }
+    })
+  }
 
-      } else if (this.state.isChosen) {
-        chooseOption(item, true)
+  handleFadeUp = () => {
+    this.setState((prevState) => {
+      const { answer } = prevState;
+      const isFinished = answer.every(letter => letter !== null)
+
+      if (isFinished) {
+        return {
+          isFinished: true
+        }
+      }
+
+    }, () => {
+      const { item, chooseOption } = this.props;
+      const { isFinished } = this.state;
+
+      if (isFinished) {
+        chooseOption(item, true);
+      }
+    })
+  }
+
+  handleLettersAnimation = () => {
+    this.setState((prevState) => {
+      const letters = prevState.letters;
+      letters.forEach(element => element.isWrong = false);
+      letters.forEach(element => element.isScaled = false);
+
+      return {
+        letters
       }
     })
   }
 
   render() {
-    const { isChosen, letters, answer } = this.state;
-    const { item } = this.props;
+    const { definition, isFinished, letters, answer } = this.state;
 
     return (
-      <div style={{display: `${isChosen ? 'none': 'block'}`, padding: '100px 20px'}}>
-        <h2>{item.definition}</h2>
+      <GameWrapper isFinished={isFinished}>
+        <Term>{definition}</Term>
 
-        <div>
-          {letters.map((letter, index) => {
-            return (
-              <p
-                id={letter}
-                key={index}
-                onClick={this.handlePicking}
-                >
-                {letter}
-              </p>
-            )
-          })}
-        </div>
+        <AnswerWrapper>
+          {answer.map((letter, index) =>
+            <AnswerLetter
+              key={index}
+              letter={letter}
+              isBlock={/\n/.test(letter)}
+              isFaded={letter}
+              onAnimationEnd={this.handleFadeUp}>
+              {letter}
+            </AnswerLetter>
+          )}
+        </AnswerWrapper>
 
-        <p>{answer.join("")}</p>
-      </div>
+        <PickWrapper>
+          {letters.map(({ letter, isScaled, isWrong }, index) =>
+            <PickLetter
+              id={letter}
+              key={index}
+              isScaled={isScaled}
+              isWrong={isWrong}
+              onClick={this.handlePicking}
+              onAnimationEnd={this.handleLettersAnimation}>
+              {letter}
+            </PickLetter>
+          )}
+        </PickWrapper>
+
+      </GameWrapper>
     )
   }
 };
+
+
+const shake = keyframes`
+  0% {
+    transform: translateX(0px);
+    opacity: 0.5;
+  }
+
+  25% {
+    transform: translateX(3px);
+  }
+
+  50% {
+    transform: translateX(-3px);
+  }
+
+  75% {
+    transform: translateX(3px);
+  }
+
+  100% {
+    transform: translateX(0px);
+    opacity: 1
+  }
+`;
+
+const scale = keyframes`
+  from {
+    transform: scale(0.7);
+  }
+
+  70% {
+    transform: scale(1.2);
+  }
+
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+`;
+
+const fadeUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 30%, 0);
+  }
+
+  60% {
+    opacity: 0.9;
+    transform: translate3d(0, 0, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+const popIn = keyframes`
+  from {
+    opacity: 0;
+    transform: scale3d(1, 1, 1);
+  }
+
+  20% {
+    opacity: 1;
+  }
+
+  50% {
+    transform: scale3d(1.15, 1.15, 1.15);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale3d(1, 1, 1);
+  }
+`
+
+const GameWrapper = styled.div`
+  display: ${({isFinished}) => isFinished ? 'none' : 'block'};
+  padding-top: 20vh;
+  width: 80vw;
+  margin: 0 auto
+`;
+
+const Term = styled.p`
+  font-size: 3rem;
+  margin: 0;
+  margin-bottom: 1.5rem;
+  text-align: center;
+  opacity: 0;
+  animation: ${popIn} .3s linear 0.4s forwards;
+
+  @media (min-width: 768px) {
+    font-size: 4rem
+  }
+`;
+
+const AnswerWrapper = styled.div`
+  height: auto;
+  width: max-content;
+  margin: 10rem auto 5rem auto;
+
+  @media (min-width: 768px) {
+    margin: 7rem auto 5rem auto;
+  }
+`;
+
+const AnswerLetter = styled.span`
+  font-size: 2.4rem;
+  display: inline-block;
+  width: ${({ letter }) => letter === " " && "1rem"};
+
+  ${({ isBlock }) => isBlock && css`
+    display: block;
+    height: 1rem;
+  `};
+
+  ${({ letter }) => !letter && css`
+    height: calc(2.4rem * 1.333);
+    width: 1rem;
+    margin: 0 0.2rem;
+    border-bottom: 1px solid white;
+  `}
+
+  ${({ isFaded }) => isFaded && css`
+    animation: ${fadeUp} .3s linear
+  `}
+
+  @media (min-width: 768px) {
+    font-size: 2.8rem;
+
+    ${({ letter }) => !letter && css`
+      height: calc(2.8rem * 1.333);
+      width: 1.5rem;
+      margin: 0 0.3rem;
+      border-bottom: 1px solid white
+    `}
+  }
+`;
+
+const PickWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 23rem;
+  margin: 0 auto
+`;
+
+const PickLetter = styled.span`
+  padding: 0.5rem 0;
+  width: 3.7rem;
+  text-align: center;
+  border: 1px solid white;
+  border-radius: 7px;
+  font-size: 2.4rem;
+  text-transform: capitalize;
+
+  ${({ isScaled }) => isScaled && css`
+    animation: ${scale} 0.4s linear both;
+  `};
+
+  ${({ isWrong }) => isWrong && css`
+    animation: ${scale} 0.3s linear, ${shake} 0.5s linear;
+  `}
+`;
 
 export default ArrayLetters
--- src/components/game/SelectFalseOrTrue.js
+++ src/components/game/SelectFalseOrTrue.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+import { colors, fonts } from '../../assets/styles/GlobalStyles';
+
 
 class SelectFalseOrTrue extends Component {
   state = {
     isChosen: false,
+    answer: "",
+    term: "",
     comparisonItem: {}
   }
 
@@ -13,6 +18,7 @@ class SelectFalseOrTrue extends Component {
   createGame() {
     this.setState((prevState, props) => {
       const { item, terms } = props;
+      const term = item.term;
       const isEqual = Math.random() > 0.5 ? true : false;
       let comparisonItem;
 
@@ -23,40 +29,173 @@ class SelectFalseOrTrue extends Component {
         comparisonItem = terms[index];
       }
 
-      return { comparisonItem }
-    })
-  }
-
-  handleChosenAnswer = (answer) => {
-    this.setState({
-      isChosen: true
-    }, () => {
-      const { item, chooseOption } = this.props;
-      const { comparisonItem } = this.state;
-      const correctAnswer = item.id === comparisonItem.id;
-
-      if (answer === correctAnswer) {
-        chooseOption(item, true);
-      } else {
-        chooseOption(item, false);
+      return {
+        term,
+        comparisonItem
       }
     })
   }
 
+  handleChosenAnswer = (event) => {
+    const answer = event.target.id;
+    this.setState({
+      answer,
+      isChosen: true
+    })
+  }
+
+  handleAnimationEnd = () => {
+    if (this.state.answer) {
+      this.setState({
+        isFinished: true
+      }, () => {
+        const { item, chooseOption } = this.props;
+        const { answer, comparisonItem } = this.state;
+        const correctAnswer = item.id === comparisonItem.id;
+        const userAnswer = answer === 'correct' ? true : false;
+
+        if (userAnswer === correctAnswer) {
+          chooseOption(item, true);
+        } else {
+          chooseOption(item, false);
+        }
+      })
+    }
+  }
+
   render() {
-    const { isChosen, comparisonItem } = this.state;
-    const { item } = this.props;
+    const { term, isChosen, isFinished, comparisonItem } = this.state;
 
     return (
-      <div style={{display: `${isChosen ? 'none': 'block'}`, padding: '100px 20px'}}>
-        <h2>{item.term}</h2>
-        <h3>{comparisonItem.definition}</h3>
-        <button onClick={() => this.handleChosenAnswer(false)}>false</button>
-        <button onClick={() => this.handleChosenAnswer(true)}>true</button>
-      </div>
+      <GameWrapper isFinished={isFinished}>
+        <Question
+          isChosen={isChosen}
+          onAnimationEnd={this.handleAnimationEnd}>
+          <Term>{term}</Term>
+          <Definition>{comparisonItem.definition}</Definition>
+        </Question>
+        <ButtonsWrapper>
+          <Button
+            id="wrong"
+            onClick={this.handleChosenAnswer}>
+            false</Button>
+          <Button
+            id="correct"
+            onClick={this.handleChosenAnswer}>
+            true</Button>
+        </ButtonsWrapper>
+      </GameWrapper>
     )
   }
 }
 
+
+const flipOut = keyframes`
+  from {
+    transform: perspective(400px);
+  }
+
+  30% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+    opacity: 1;
+  }
+
+  to {
+    transform: perspective(400px) rotate3d(1, 0, 0, 40deg);
+    opacity: 0;
+  }
+`;
+
+const flipIn = keyframes`
+  from {
+    opacity: 0;
+    transform: perspective(400px) rotate3d(1, 0, 0, 40deg);
+  }
+
+  30% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+    opacity: 1;
+  }
+
+  to {
+    transform: perspective(400px);
+    opacity: 1;
+  }
+`;
+
+const GameWrapper = styled.div`
+  display: ${({ isFinished }) => isFinished ? 'none' : 'block'};
+  padding-top: 20vh;
+  width: 70vw;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    width: 30rem;
+  }
+`;
+
+const Question = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: max-content;
+  min-height: 50vh;
+  width: 100%;
+  max-width: 80vw;
+  margin: 0 auto;
+  background: rgba(65, 33, 160, 0.5);
+  border-radius: 4rem;
+  padding: 2rem;
+  animation: ${flipIn} 0.5s linear;
+
+  ${({ isChosen }) => isChosen && css`
+    animation: ${flipOut} 0.5s linear forwards
+  `};
+`;
+
+const Term = styled.p`
+  font-size: 3rem;
+  margin: 0;
+  margin-bottom: 1.5rem;
+  text-align: center;
+`;
+
+const Definition = styled.p`
+  font-size: 2.2rem;
+  margin: 0;
+  text-align: center;
+`;
+
+const ButtonsWrapper = styled.div`
+  margin: 40px auto 60px auto;
+  display: flex;
+  justify-content: space-between;
+  width: 60vw;
+  max-width: 300px;
+`;
+
+const Button = styled.button`
+  color: ${colors.white};
+  border: 1px solid ${colors.white};
+  font-family: ${fonts.family};
+  display: block;
+  width: 9rem;
+  height: 4rem;
+  margin: 0;
+  text-align: center;
+  font-size: 1.4rem;
+  background: transparent;
+  border-radius: 50px;
+  transition: transform .2s;
+
+  &:focus {
+    transform: translateY(2px);
+  }
+
+  @media (min-width: 768px) {
+    width: 13rem;
+  }
+`;
 
 export default SelectFalseOrTrue
